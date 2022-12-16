d4b4b52889778ac9144ca4a2846d715eab55bf06
Sterling <s.evans723@gmail.com>
2022-12-15 23:27:31 -0500

    added some mouseover attributes and other functions!

--- index.js
+++ index.js
@@ -3,40 +3,56 @@ let homeScore = 0
 
 let awayScoreEl = document.getElementById("away-points")
 let awayScore = 0
+let words = document.querySelector("#points-words")
+
+let player = {
+  name: "Sterlz",
+}
 
 function reset() {
   homeScoreEl.textContent = 0
   awayScoreEl.textContent = 0
   homeScore = 0
   awayScore = 0
+  words.textContent = "Lets play again!"
 }
 
 function addOneHome() {
   homeScore += 1
   homeScoreEl.textContent = homeScore
+  words.textContent = "Way to make your freethrows," + " " + player.name + "!"
 }
 
 function addTwoHome() {
   homeScore += 2
   homeScoreEl.textContent = homeScore
+  words.textContent = "Nice 2-pointer," + " " + player.name + "!"
 }
 
 function addThreeHome() {
   homeScore += 3
   homeScoreEl.textContent = homeScore
+  words.textContent = "Nice 3-pointer," + " " + player.name + "!"
 }
 
 function addOneAway() {
   awayScore += 1
   awayScoreEl.textContent = awayScore
+  words.textContent = "Way to make your freethrows," + " " + player.name + "!"
 }
 
 function addTwoAway() {
   awayScore += 2
   awayScoreEl.textContent = awayScore
+  words.textContent = "Nice 2-pointer," + " " + player.name + "!"
 }
 
 function addThreeAway() {
   awayScore += 3
   awayScoreEl.textContent = awayScore
+  words.textContent = "Nice 3-pointer," + " " + player.name + "!"
+}
+
+function clearText() {
+  words.textContent = ""
 }
